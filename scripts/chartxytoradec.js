131fe1e3106d56cddf0c68bff27999c9d1ea1eda
Herr_Alien <garone80@yahoo.com>
2015-04-13 23:21:28 +0300

    take picture margin into account

--- scripts/chartxytoradec.js
+++ scripts/chartxytoradec.js
@@ -3,13 +3,47 @@
 var ChartXYToRADec = {
     imageElem : document.getElementById ("chart"),
     centerXYCoordsRatios : [600.0/1200, 765.0/1500],
+    usefullWidthRatio : (600 - 18) / 600.0,
     centerRADecCoords : [],
-    ChartXYToRADec : [],
+    centerXYCoords : [],
     focalLength : 0,
     
+    onCoordsChanged : null,
+    
+    getRADec : function (xy) {
+        var dx = xy[0] - ChartXYToRADec.centerXYCoords[0];
+        var dy = ChartXYToRADec.centerXYCoords[1] - xy[1];
+        
+        var dra = 180 / Math.PI * Math.atan (dx / ChartXYToRADec.focalLength);
+        var ddec = 180 / Math.PI * Math.atan (dy / ChartXYToRADec.focalLength);
+        
+        return [ChartXYToRADec.centerRADecCoords[0] - dra, ChartXYToRADec.centerRADecCoords[1] + ddec];
+    },
+    
+    handleMouseMovement : function (evt) {
+        var event = evt|| window.event; // IE-ism
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+        
+        if (ChartXYToRADec.onCoordsChanged)
+            ChartXYToRADec.onCoordsChanged (ChartXYToRADec.getRADec([event.pageX, event.pageY]));
+    },
+
     init : function (centerRADec, fov_mins) {
         ChartXYToRADec.centerRADecCoords = centerRADec;
-        var offset = [ChartXYToRADec.imageElem.x, ChartXYToRADec.imageElem.y];
+        var offset = [ChartXYToRADec.imageElem.x || ChartXYToRADec.imageElem.offsetLeft, 
+                      ChartXYToRADec.imageElem.y || ChartXYToRADec.imageElem.offsetTop];
         ChartXYToRADec.centerXYCoords = [ChartXYToRADec.imageElem.width * ChartXYToRADec.centerXYCoordsRatios[0] + offset[0],
                                          ChartXYToRADec.imageElem.height * ChartXYToRADec.centerXYCoordsRatios[1] + offset[1]];
 
@@ -17,15 +51,7 @@ var ChartXYToRADec = {
           ChartXYToRADec.imageElem.width / 2 = R * tan (fov/2) =>
           R =  ChartXYToRADec.imageElem.width / (2 * tan (fov/2))
     */
-        ChartXYToRADec.focalLength = ChartXYToRADec.imageElem.width / (2 * Math.tan (fov_mins * Math.PI /(2 * 60.0 * 180)));
-    },
-    
-    getRADec : function (xy) {
-        // dRA = atan (x - center.x, focalLength)
-        var dra = 180 / Math.PI * atan (xy[0] - ChartXYToRADec.ChartXYToRADec[0], ChartXYToRADec.focalLength);
-        // dDec = atan (y - center.y, focalLength)
-        var ddec = 180 / Math.PI * atan (xy[1] - ChartXYToRADec.ChartXYToRADec[1], ChartXYToRADec.focalLength);
-        
-        return [ChartXYToRADec.centerRADecCoords[0] + dra, ChartXYToRADec.centerRADecCoords[1] + ddec];
+        ChartXYToRADec.focalLength = (0.5 * ChartXYToRADec.imageElem.width * ChartXYToRADec. usefullWidthRatio) / ( Math.tan(((0.5 * fov_mins)/60) * Math.PI / 180 ));
+        ChartXYToRADec.imageElem.onmousemove = function (e) { ChartXYToRADec.handleMouseMovement(e); }
     }
 };
