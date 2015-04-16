9d6fdae7c122b54f252788179017722781554401
Herr_Alien <devnull@localhost>
2015-04-16 20:05:21 +0300

    detail search tree

--- scripts/photometrytable.js
+++ scripts/photometrytable.js
@@ -2,18 +2,79 @@
 
 var PhotmetryTable = {
     
-    centerCoords : [],
-    fov : 0,
-    
-    searchTree : {
+    searchTree : {        
+        root : null,
+        
+        settings : {
+            maxFovPerNode : 10 // this is in arcmins
+        },
+        
         getClosestStar : function(ra, dec) {
             var foundStar = { "ra": ra, "dec": dec, "mag" : 0 };
             // this gets filled with the result of the search
+            
+            var closestStars = PhotmetryTable.searchTree.root.GetStarsAt ([ra, dec]);
+            
+            // loop through them, return the one closest to input param
+            var i = 0;
+            for (i = 0; i < )
+            
             return foundStar;
         },
         
-        init : function (coords, span, mag) {
+        node : function (coords, fov, stars) {
+            this.sons = [];
+            this.coords = coords;
+            this.fov = fov * 1.1; // this is to overlap a bit with neighbors
+            this.stars = [];
+            
+            this.hasCoords = function (radec) {
+                var halfFOV = 0.5 * this.fov; 
+                return ((radec[0] > coords[0] - halfFOV && radec [0] < coords[0] + halfFOV) &&
+                        (radec[1] > coords[1] - halfFOV && radec [1] < coords[1] + halfFOV))
+            };
+            
+            this.GetStarsAt = function (radec) {
+                if (this.hasCoords(radec)){
+                    if (this.stars.length != 0)
+                        return this.stars;
+                    else {
+                        var i = 0; 
+                        for (i = 0; i < this.sons.length; i++) {
+                            var sonsStars = this.sons [i].GetStarsAt (radec);
+                            if (sonsStars.length > 0)
+                              return sonsStars;
+                        }
+                    }
+                }
+                return [];
+            };
+            
+            // do an initial fill here; this acts as filter for sons
+            var i = 0;
+            for (; i < stars.length; i++)
+                if (this.hasCoords ( [stars[i]["ra"], stars[i]["dec"] ) )
+                    this.stars.append (stars[i]);
+            
+            // now that we bit a little more, we can set back the FOV
+            this.fov = fov;
+            if (this.fov > PhotmetryTable.searchTree.settings.maxFovPerNode * 1.1) {
+                var sonsFOV = this.fov * 0.5;
+                this.sons.append (new PhotmetryTable.searchTree.node ( [coords[0] + sonsFOV, coords[1] + sonsFOV] , sonsFOV, this.stars));
+                this.sons.append (new PhotmetryTable.searchTree.node ( [coords[0] - sonsFOV, coords[1] - sonsFOV] , sonsFOV, this.stars));
+                this.sons.append (new PhotmetryTable.searchTree.node ( [coords[0] + sonsFOV, coords[1] - sonsFOV] , sonsFOV, this.stars));
+                this.sons.append (new PhotmetryTable.searchTree.node ( [coords[0] - sonsFOV, coords[1] + sonsFOV] , sonsFOV, this.stars));
+                this.stars = []; // all stars were passed along to the sons
+            }
+        },
+        
+        init : function (coords, fov, mag) {
             alert ("Init at " + cords[0] + ", " + coords[1]);
+            // T0DO: prepare a list of stars, up to mag
+            var stars = [];
+            
+            // then create the quad tree
+            PhotmetryTable.searchTree.root = new PhotmetryTable.searchTree.node (coords, fov, stars);
         }
     },
     
@@ -55,15 +116,12 @@ var PhotmetryTable = {
     },
 
     init : function (starName, FOV, limittingMag) {
-        PhotmetryTable.fov = FOV;
         var xmlHttpReq = new XMLHttpRequest();
         xmlHttpReq.onreadystatechange = function() {
             if(xmlHttpReq.readyState == 4) {
                 var doc =  xmlHttpReq.responseText;
-
-                PhotmetryTable.centerCoords  = PhotmetryTable.AAVSO.GetCenter (doc);
-
-                PhotmetryTable.searchTree.init (PhotmetryTable.centerCoords, FOV, limittingMag);
+                var centerCoords  = PhotmetryTable.AAVSO.GetCenter (doc);
+                PhotmetryTable.searchTree.init (centerCoords, FOV, limittingMag);
             }
         }
         xmlHttpReq.open(PhotmetryTable.AAVSO.config.method, PhotmetryTable.AAVSO.config.vsxFormURL+ encodeURI(starName), true);
