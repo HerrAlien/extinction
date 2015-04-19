a3ad6cba508ebadb0511cc80bcad91ee5867c991
Herr_Alien <devnull@localhost>
2015-04-19 19:01:17 +0300

    Added utility for com[utations

---
+++ scripts/computations.js
@@ -0,0 +1,66 @@
+/*
+Extinction-O-Meter - an HTML & JavaScript utility to compute atmospheric extinction.
+               
+Copyright 2015  Herr_Alien <garone80@yahoo.com>
+                
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+                
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+                
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/agpl.html
+*/
+
+var Computations = {
+    
+    JD : function (timeStr) {
+        
+        var d = new Date (Date.parse (timeStr));
+        
+        var a = Math.floor((14 - (d.getMonth() + 1)) / 12);
+        var y = d.getFullYear() + 4800 - a;
+        var m = d.getMonth() + 1 + 12 * a - 3;
+        
+        var JDN = d.getDate() + Math.floor ((153 * m + 2) / 5) + 365 * y + 
+                  Math.floor (y / 4) - Math.floor (y / 100) + Math.floor (y / 400) - 32045;
+            
+        return  JDN + (d.getHours() - 12) / 24 + d.getMinutes() / 1440 + d.getSeconds() / 86400;
+    },
+    
+    LST : function (currentJD, longitude) {
+        var nonNormedLST = 280.46061837 + 360.98564736629 * (currentJD - 2451545) + longitude;
+        return nonNormedLST - 360 * Math.floor (nonNormedLST / 360);
+    },
+    
+    Alt : function (_ra, _dec, timeStr, _lat, _long) {
+
+        var deg2rad = Math.PI / 180;
+        var currentJD = 0;
+        try { // assume a JD first
+            currentJD = parseFloat (timeStr);
+        } catch (err) {
+            currentJD = Computations.JD (timeStr);
+        }
+        
+        var lst = Computations.LST (currentJD, _long) * deg2rad;
+        var ra = _ra * deg2rad;
+        var dec = _dec * deg2rad;
+        var lat = _lat * deg2rad;
+        var long = _long * deg2rad;
+
+        return Math.asin (Math.sin (dec) * Math.sin (lat) + Math.cos (dec) * Math.cos (lat) * Math.cos (lst - ra)) * 180 / Math.PI;
+    },
+    
+    Airmass : function (alt) {
+        var cos_zt = Math.cos((90 - alt) * Math.PI / 180);
+
+        return (1.002432 * Math.pow (cos_zt, 2) + 0.148386 * cos_zt + 0.0096467) / 
+               (Math.pow (cos_zt, 3) + 0.149864 * Math.pow(cos_zt, 2) + 0.0102963 * cos_zt + 0.000303978);
+    }
+};
