fc3fa0c868e3b366de3887da3f06921466835f0a
herr_alien <devnull@localhost>
2015-06-08 21:16:00 +0300

    fix background script

--- chromeapp.js
+++ chromeapp.js
@@ -1,6 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(
     function() {
         var createdWindow = chrome.app.window.create('index.html', { id: 'main', bounds: { width: 1280, height: 720 } });
-        createdWindow.scrollbars.visible = true;
     }
 );
