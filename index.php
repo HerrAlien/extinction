dc01145c139dd615bd38da3268eb1727da030cc3
herr_alien <devnull@localhost>
2015-08-05 23:25:51 +0300

    detect protocol - same origin policy

--- index.php
+++ index.php
@@ -88,10 +88,11 @@ if ($isMobileOrMac)
             container.innerHTML = container.innerHTML + 
                                  ' | Logged in as <?php echo $user->getNickname() ?> | <a href="https://www.google.com/accounts/ManageAccount" target="_blank">My Account</a> | <a href="<?php echo UserService::createLogoutURL("http://extinction-o-meter.appspot.com") ?>">Sign out</a>';
         
+            var protocol = document.location.protocol;
             // have the PHP re-do the URLs to point to us
-            PhotmetryTable.AAVSO.configFromStarName.url = "http://extinction-o-meter.appspot.com/index.php?";
-            PhotmetryTable.AAVSO.vsxConfig.url = "http://extinction-o-meter.appspot.com/index.php?";
-            Hipparcos.config.url = "http://extinction-o-meter.appspot.com/index.php";
+            PhotmetryTable.AAVSO.configFromStarName.url = protocol + "//extinction-o-meter.appspot.com/index.php?";
+            PhotmetryTable.AAVSO.vsxConfig.url = protocol + "//extinction-o-meter.appspot.com/index.php?";
+            Hipparcos.config.url = protocol + "//extinction-o-meter.appspot.com/index.php";
         
         })();
     </script><?php        
