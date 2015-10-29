40596f5d8dd3e2d493e3626a861840d594cb1f3f
herr_alien <devnull@localhost>
2015-10-29 17:39:16 +0200

    css fun

--- scripts/starsselection.js
+++ scripts/starsselection.js
@@ -20,7 +20,6 @@ along with this program.  If not, see https://www.gnu.org/licenses/agpl.html
 
 var StarsSelection = {
     currentlyHoveredStar : null,
-    imageElem : document.getElementById ("chart"),
     activeSelector : null,
 	selectionJustActivated: false,
    
