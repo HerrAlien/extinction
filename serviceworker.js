85a383ee113bce90f48e218845c4d9ab1c2e47c4
herr_alien <devnull@localhost>
2018-01-29 12:56:40 +0200

    better iOS support

--- serviceworker.js
+++ serviceworker.js
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see https://www.gnu.org/licenses/agpl.html */
 
 (function() {
-var CACHE_NAME = 'Cache-for-extinction-o-meter-v1';
+var CACHE_NAME = 'Cache-for-extinction-o-meter-v2';
 var urlsToCache = [
 "/"
 ];
