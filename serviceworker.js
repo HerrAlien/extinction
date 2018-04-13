e1e25747df934123a3fb7438a2ced3ba7f513e32
herr_alien <devnull@localhost>
2018-04-13 19:22:23 +0300

    add the structured data

--- serviceworker.js
+++ serviceworker.js
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see https://www.gnu.org/licenses/agpl.html */
 
 (function() {
-var CACHE_NAME = 'Cache-for-extinction-o-meter-v5';
+var CACHE_NAME = 'Cache-for-extinction-o-meter-v6';
 var urlsToCache = [
 "/"
 ];
